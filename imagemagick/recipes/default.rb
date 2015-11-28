a42782d0033a773811743f4596f03193864e2e96
“Paulo <phsoares.ita@gmail.com>
2015-11-28 20:59:39 -0200

    other packages

---
+++ imagemagick/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: imagemagick
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "imagemagick" do
+  action :install
+end
