82a929bd0f4630927d658b8084be0bf14e61ed9d
“Paulo <phsoares.ita@gmail.com>
2015-11-28 19:14:23 -0200

    installs nodejs and bower

---
+++ rails-bower/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: rails-bower
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "nodejs" do
+  action :install
+end
+
+execute "install bower" do
+  command "sudo npm install bower -g"
+  action :run
+end
